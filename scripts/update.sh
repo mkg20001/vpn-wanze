90618db4c26f7dca804fdf231f3e528835fbb59c
Maciej Krüger <mkg20001@gmail.com>
2019-04-11 13:14:59 +0200

    feat: add motd

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+set -e
+
+# update motd
+echo -e "'VPN Wanze' VPN-Server - Entwickelt von Maciej Krüger\n\nVerwaltung:\n\tsudo wanze setup - Server einrichten\n\tsudo proxy status - Server Status anzeigen\n\tsudo wanze-update - Server software aktualisieren\n" > /etc/motd
+
 wanze refresh
