3642401b7b0b37fcc92c66ed18e3e58a38fb4ffd
Maciej Krüger <mkg20001@gmail.com>
2019-06-04 09:46:47 +0200

    fix: lower the mtu

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,6 @@
 set -e
 
 # update motd
-echo -e "'VPN Wanze' VPN-Server - Entwickelt von Maciej Krüger\n\nVerwaltung:\n\tsudo wanze setup - Server einrichten\n\tsudo proxy status - Server Status anzeigen\n\tsudo wanze-update - Server software aktualisieren\n" > /etc/motd
+echo -e "'VPN Wanze' VPN-Server - Entwickelt von Maciej Krüger\n\nVerwaltung:\n\tsudo wanze setup - Server einrichten\n\tsudo wanze status - Server Status anzeigen\n\tsudo wanze-update - Server software aktualisieren\n" > /etc/motd
 
 wanze refresh
